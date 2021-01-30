49f0daf720dce1cbb17294fb26f716242f9fc210
Giuliano Oliveira <giuliano.programador@gmail.com>
2021-01-30 11:19:42 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div
+      v-for="country in countries"
+      :key="country.key"
+      class="country"
+    >
+      <h1>{{country.key}}</h1>
+      <hr />
+      <h2>{{country.sources.domestic[0]}}</h2>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from './components/HelloWorld.vue'
+import { Vue } from 'vue-property-decorator'
 
-@Component({
-  components: {
-    HelloWorld
+export default class App extends Vue {
+  get countries () {
+    const requireContext = require.context(
+      './constants/countries',
+      true,
+      /(.*)\.yml/
+    )
+    return requireContext.keys().map((fileName) => {
+      const key: string = fileName.split('.')[1].replace('/', '')
+      return {
+        key: key,
+        ...requireContext(fileName)[key]
+      }
+    })
   }
-})
-export default class App extends Vue {}
+}
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+}
+.country{
+  border:1px solid black;
+  margin:20px;
 }
 </style>
